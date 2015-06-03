3a094f878d66d78930cf87990d3da022806865ed
JoaoPedroPinheiro <joaopedropinheiro.lib@gmail.com>
2015-06-03 20:18:01 +0100

    MessageService interface created

---
+++ src/agent/communication/MessageService.java
@@ -0,0 +1,30 @@
+package agent.communication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import trade.Ask;
+import trade.Bid;
+import agent.BuyerAgentBDI;
+import agent.OperatorAgentBDI;
+import agent.SellerAgentBDI;
+import jadex.bridge.service.annotation.Service;
+import jadex.commons.future.IFuture;
+
+@Service
+public interface MessageService {
+	
+	public IFuture<Void> serviceBody(int value);
+	
+	public Map<String, SellerAgentBDI> SellerList = new HashMap<String, SellerAgentBDI>();
+	public Map<String, BuyerAgentBDI> BuyerList = new HashMap<String, BuyerAgentBDI>();
+	
+	public void start(OperatorAgentBDI m);
+	public OperatorAgentBDI getMarket();
+	
+	public void register(BuyerAgentBDI b);
+	public void register(SellerAgentBDI s);
+	
+	public void bid(Bid b);
+	public void ask(Ask a);
+}
